743f35a04abad59f11c3d0334fa34bc9f10b938c
Drew Ruselowski <druselow@gmail.com>
2015-04-16 16:22:57 -0700

    added notes to work through ex2

---
+++ DrewSaven/Recursion/SandR_Q2.js
@@ -0,0 +1,9 @@
+//first we need to figure out how to input a tree
+
+//We will need to scan through the different nodes of the tree to figure out
+// which nodes have children.  
+
+//Make sure that a node has no more than 2 kids
+//report the degree of the tree. (we could do this a  number of ways--
+//for instance, we could keep track of how many levels we go in a separate
+//array, and ultimately report back the length of the array.
