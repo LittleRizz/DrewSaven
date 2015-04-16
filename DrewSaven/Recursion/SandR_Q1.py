29d1691de987c8128f72f6a06728c163ad142976
Saven Roybal <SavenR@users.noreply.github.com>
2015-04-16 16:21:12 -0700

    Commented up Q1

---
+++ DrewSaven/Recursion/SandR_Q1.py
@@ -0,0 +1,20 @@
+# Pascal's triangle
+# accepts n, the outpul level, as an integer
+# output prints
+
+# Create a function
+function pascalT(level):
+
+
+  # Create a helper function:
+
+  # Create a base case to break the recursion
+  if level <1:
+  	return
+  	
+  # Create a step that uses the prior array to
+  # generate the next array and prints it.
+
+  # Need a way to start with printing 1
+
+
