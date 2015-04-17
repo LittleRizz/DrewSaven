3723a083c429d1b3c55f80094b16c8719958d806
Drew Ruselowski <druselow@gmail.com>
2015-04-17 09:26:41 -0700

    Final Triangle

--- DrewSaven/Recursion/SandR_Q1.py
+++ DrewSaven/Recursion/SandR_Q1.py
@@ -1,20 +1,25 @@
-# Pascal's triangle
-# accepts n, the outpul level, as an integer
-# output prints
+def BitchAssTriangle (rowNum):
+   #first we write a breakout sequence.  Like a fuckin boss.
+   if rowNum == 1:
+      return [1]
+      #see, this will break us out, and at the same time put a pretty 
+      #top on our triangle.  Cause that's how we roll.
+   else:
+      #since every line starts (and ends) with 1, we can make that a default value.
+      currentLine = [1]
+      #now we need a function that will compare the previous line.
+      #we're gonna go inception, and use rowNum-1, since the line above is shorter
+      previousLine = BitchAssTriangle(rowNum-1)
+      #we want to iterate through the previous line, 
+      #but leave the last number, since it's always 1.
+      for i in range(len(previousLine)-1):
+         #now since previousLine is technically an array, we can call the values
+         #that we need to perform artihmatic on.  We will append those values
+         #to our currentLine.
+         currentLine.append(int(previousLine[i] + previousLine[i+1]))
+      #then we tack on a nice happy 1 onto the end of this bisnizzy.
+      currentLine += [1]
+   print previousLine
+   return currentLine
 
-# Create a function
-function pascalT(level):
-
-
-  # Create a helper function:
-
-  # Create a base case to break the recursion
-  if level <1:
-  	return
-  	
-  # Create a step that uses the prior array to
-  # generate the next array and prints it.
-
-  # Need a way to start with printing 1
-
-
+print(BitchAssTriangle(4))
