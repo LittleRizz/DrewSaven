c344474258b4c6c26fb8c49802c35bb92659378f
Drew Ruselowski <druselow@gmail.com>
2015-04-16 23:51:34 -0700

    updated triangle code and what have you

--- DrewSaven/Recursion/SandR_Q1.py
+++ DrewSaven/Recursion/SandR_Q1.py
@@ -2,18 +2,38 @@
 # accepts n, the outpul level, as an integer
 # output prints
 
-# Create a function
-function pascalT(level):
+# Create a function that expands on itself, from 1 to n
+def pascalT(n):
 
+  # Create a helper function
+  def helper(array, n): 
+  iAr = [1]
+  
+    # Create a base case to break the recursion - n will have to be the limit
+    # Will a
+  if len(iAR) == n:
+    return
+  for i in range(len(array)):
+    iAr.append(array[i]+array[i+1:i+2])
 
-  # Create a helper function:
-
-  # Create a base case to break the recursion
-  if level <1:
-  	return
   	
   # Create a step that uses the prior array to
   # generate the next array and prints it.
+  
+  #with each loop of the recursion, we're going to want to do two things:
+  #create a new array that is one longer, and use the values of the previous array to populate the new array
+  
+  
+  
+  # May need to always append 1 first, this would
+  # also solve the initial level problem
+  
+  # Then, increment based on the length of the
+  # parameter array.
+  
+  
+  # Need to use ar[i:i+1] notation to handle the 
+  # end of the parameter array neatly
 
   # Need a way to start with printing 1
 
--- DrewSaven/Recursion/SandR_Q2.js
+++ DrewSaven/Recursion/SandR_Q2.js
@@ -7,3 +7,49 @@
 //report the degree of the tree. (we could do this a  number of ways--
 //for instance, we could keep track of how many levels we go in a separate
 //array, and ultimately report back the length of the array.
+
+// Create a tree object for testing purposes
+function BinTree(identity) {
+  var tree = {},
+      tree.identity = identity,
+      tree.children = [];
+      
+      function tree.addChild( self , val ){
+        if
+        var holder = tree( val );
+        tree.children.push(holder);
+      }
+      
+      
+}
+
+class Tree(object):
+    def __init__(self, value):
+        self.children = []
+        # More code here
+        # Creates a value for holding the value
+        self.value = value
+    def prettyPrint(self):
+		print "Node: {0}".format(self.value)
+		if len(self.children):
+		    for i in self.children:
+		        i.prettyPrint()
+		        # pass
+
+    def add_child(self, value):
+    	# Creates a new tree 
+    	if (self.children.length <2 ) {
+    	  
+    	holder = Tree(value)
+        self.children.append(holder)
+        # pass
+
+    def contains(self, target):
+    	# Should check the entire tree
+    	if self.value == target:
+    		return True
+        else:
+        	for i in self.children:
+	        	if i.contains(target):
+	        		return True
+		return False
